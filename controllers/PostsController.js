4fc471dd5f2c5fbf71b2205588a85b99b9b2d58d
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-12-07 17:26:09 +0100

    Milestone 4

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -49,147 +49,194 @@ const index = (req, res) => {
 // show: tramite il parametro dinamico che rappresenta lo slug del post, ritornerà un json con i dati del post
 const show = (req, res) => {
 
-    const post = posts.find(post => post.slug.toLowerCase() === req.params.slug.toLowerCase());
-    if (!post) {
-        return res.status(404).json({
-            error: `404! Not found`
-        })
-    }
+    //console.log(req.params.id);
+    const id = req.params.id;
+    console.log(id);
 
-    return res.status(200).json({
-        data: post
-    })
-}
+    // prepare the sql query to get the posts by its id
+    const sql = 'SELECT * FROM posts WHERE id=?'
 
-// Creare inoltre un filtro in querystring per tag, che ritorna in formato json tutti i post che hanno quei tag
-const filter = (req, res) => {
-    const { tag } = req.query;
-    console.log(tag);
-
-
-    if (!tag) {
-        return res.status(400).json({ error: 'Nessun tag specificato' });
-    }
-
-    // utilizzo filter per filtrare i post che includono tag
-    const postsFiltrati = posts.filter(post => post.tags.includes(tag));
-    console.log(postsFiltrati);
-
-
-    return res.status(200).json({
-        data: postsFiltrati
-    });
-}
-
-
-// Aggiungi il metodo store per la creazione di un nuovo post
-const store = (req, res) => {
-    const post = {
-        title: req.body.title,
-        slug: req.body.slug,
-        content: req.body.content,
-        image: req.body.image,
-        tags: [req.body.tags]
-    }
-
-    posts.push(post);
-
-    fs.writeFileSync('./db/db.js', `module.exports = ${JSON.stringify(posts, null, 4)}`)
-
-    res.json({
-        status: 201,
-        data: posts,
-        counter: posts.length
-    })
-}
-
-// Update post
-const update = (req, res) => {
-    // prima devo trovare il post da modificare, attraverso il titolo
-    const { title } = req.params;
-
-
-    // ora cerco l'indice del post attraverso il titolo
-    const postIndex = posts.findIndex(post => post.title.toLowerCase() === title.toLowerCase());
-
-    // verifico se esiste un post con quel titolo
-    if (postIndex === -1) {
-        return res.status(404).json({
-            status: 404,
-            message: "Il post che vuoi modificare non è stato trovato"
-        })
-    }
-
-    // vado a modificare direttamente il post a quell'indice
-    posts[postIndex] = {
-        title: req.body.title,
-        slug: req.body.slug,
-        content: req.body.content,
-        image: req.body.image,
-        tags: [req.body.tags]
-    }
-
-    // aggiorno il file 
-    fs.writeFileSync('./db/db.js', `module.exports = ${JSON.stringify(posts, null, 4)}`)
-
-    res.json({
-        status: 201,
-        message: "Post modificato con successo",
-        data: posts,
-        counter: posts.length
-    })
-}
-
-// delete post
-const destroy = (req, res) => {
-
-    console.log(req.params);
-
-
-    //1. take the resource id from the request
-    const id = req.params.id
-
-    //2. prepare the sql query to delete the record form the db
-    const sql = 'DELETE FROM posts WHERE id=?'
-
-    //3. perform the prepared statement query
+    // prepare another query to get all the tags associated with that posts
+    const tagsSql = `
+        SELECT tags.*
+        FROM tags
+        JOIN post_tag ON tags.id = post_tag.tag_id
+        WHERE post_tag.post_id = ?
+    `
+    // exectute the first query to get the pizza by its id
     connection.query(sql, [id], (err, results) => {
-        console.log(err, results);
-        if (err) return res.status(500).json({ error: err })
-        //4. handle a 404 error if the record is not found
-        if (results.affectedRows === 0) return res.status(404).json({ error: `404! No posts found with the this id: ${id}` })
+        if (err) return res.status(500).json({ error: err });
+        // handle the 404 error
+        if (!results[0]) return res.status(404).json({ error: `404! Not found` })
 
-        return res.json({ status: 204, affectedRows: results.affectedRows })
+        // perpare the response data
+        const post = results[0]
+        console.log('post obj', post);
 
-    })
+        // execute the second query to get all the tags associated with that post
+        connection.query(tagsSql, [id], (err, tagsResults) => {
+            // handle 500 error
+            if (err) return res.status(500).json({ error: err })
+            console.log('👉', tagsResults);
 
-    /* // trova il post dal titolo
-    const post = posts.find(post => post.title.toLowerCase() === req.params.title.toLowerCase())
+            post.tags = tagsResults;
 
-    // controlliamo se il post è presente
-    if (!post) {
-        return res.status(404).json({ error: "No post found with that title" })
+
+            const responseData = {
+                data: post,
+            }
+
+            console.log(responseData);
+
+            // return the post
+            res.status(200).json(responseData);
+
+        })
+
+        /* const post = posts.find(post => post.slug.toLowerCase() === req.params.slug.toLowerCase());
+        if (!post) {
+            return res.status(404).json({
+                error: `404! Not found`
+            })
+        }
+    
+        return res.status(200).json({
+            data: post
+        })
+        */
+
+     })
     }
 
-    // rimuoviamo il post restituendo un nuovo array senza quel post
-    const newPosts = posts.filter(post => post.title.toLowerCase() !== req.params.title.toLowerCase())
+    // Creare inoltre un filtro in querystring per tag, che ritorna in formato json tutti i post che hanno quei tag
+    const filter = (req, res) => {
+        const { tag } = req.query;
+        console.log(tag);
 
-    // aggiorniamo il file 
-    fs.writeFileSync('./db/db.js', `module.exports = ${JSON.stringify(newPosts, null, 4)}`)
 
-    // restituiamo gli elementi aggiornati 
-    res.status(200).json({
-        status: 200,
-        data: newPosts,
-        counter: newPosts.length
-    }) */
-}
+        if (!tag) {
+            return res.status(400).json({ error: 'Nessun tag specificato' });
+        }
 
-module.exports = {
-    index,
-    show,
-    store,
-    filter,
-    update,
-    destroy
-}
+        // utilizzo filter per filtrare i post che includono tag
+        const postsFiltrati = posts.filter(post => post.tags.includes(tag));
+        console.log(postsFiltrati);
+
+
+        return res.status(200).json({
+            data: postsFiltrati
+        });
+    }
+
+
+    // Aggiungi il metodo store per la creazione di un nuovo post
+    const store = (req, res) => {
+        const post = {
+            title: req.body.title,
+            slug: req.body.slug,
+            content: req.body.content,
+            image: req.body.image,
+            tags: [req.body.tags]
+        }
+
+        posts.push(post);
+
+        fs.writeFileSync('./db/db.js', `module.exports = ${JSON.stringify(posts, null, 4)}`)
+
+        res.json({
+            status: 201,
+            data: posts,
+            counter: posts.length
+        })
+    }
+
+    // Update post
+    const update = (req, res) => {
+        // prima devo trovare il post da modificare, attraverso il titolo
+        const { title } = req.params;
+
+
+        // ora cerco l'indice del post attraverso il titolo
+        const postIndex = posts.findIndex(post => post.title.toLowerCase() === title.toLowerCase());
+
+        // verifico se esiste un post con quel titolo
+        if (postIndex === -1) {
+            return res.status(404).json({
+                status: 404,
+                message: "Il post che vuoi modificare non è stato trovato"
+            })
+        }
+
+        // vado a modificare direttamente il post a quell'indice
+        posts[postIndex] = {
+            title: req.body.title,
+            slug: req.body.slug,
+            content: req.body.content,
+            image: req.body.image,
+            tags: [req.body.tags]
+        }
+
+        // aggiorno il file 
+        fs.writeFileSync('./db/db.js', `module.exports = ${JSON.stringify(posts, null, 4)}`)
+
+        res.json({
+            status: 201,
+            message: "Post modificato con successo",
+            data: posts,
+            counter: posts.length
+        })
+    }
+
+    // delete post
+    const destroy = (req, res) => {
+
+        console.log(req.params);
+
+
+        //1. take the resource id from the request
+        const id = req.params.id
+
+        //2. prepare the sql query to delete the record form the db
+        const sql = 'DELETE FROM posts WHERE id=?'
+
+        //3. perform the prepared statement query
+        connection.query(sql, [id], (err, results) => {
+            console.log(err, results);
+            if (err) return res.status(500).json({ error: err })
+            //4. handle a 404 error if the record is not found
+            if (results.affectedRows === 0) return res.status(404).json({ error: `404! No posts found with the this id: ${id}` })
+
+            return res.json({ status: 204, affectedRows: results.affectedRows })
+
+        })
+
+        /* // trova il post dal titolo
+        const post = posts.find(post => post.title.toLowerCase() === req.params.title.toLowerCase())
+    
+        // controlliamo se il post è presente
+        if (!post) {
+            return res.status(404).json({ error: "No post found with that title" })
+        }
+    
+        // rimuoviamo il post restituendo un nuovo array senza quel post
+        const newPosts = posts.filter(post => post.title.toLowerCase() !== req.params.title.toLowerCase())
+    
+        // aggiorniamo il file 
+        fs.writeFileSync('./db/db.js', `module.exports = ${JSON.stringify(newPosts, null, 4)}`)
+    
+        // restituiamo gli elementi aggiornati 
+        res.status(200).json({
+            status: 200,
+            data: newPosts,
+            counter: newPosts.length
+        }) */
+    }
+
+    module.exports = {
+        index,
+        show,
+        store,
+        filter,
+        update,
+        destroy
+    }
